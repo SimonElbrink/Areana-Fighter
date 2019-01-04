97d43c71778c928a897b2fdc27367af12d1861d8
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2019-01-04 16:55:34 +0100

    RandomNumberGenerator Roll Dice

--- src/main/java/se/lexicon/simon/arena_fighter_assignment/Main.java
+++ src/main/java/se/lexicon/simon/arena_fighter_assignment/Main.java
@@ -1,7 +1,11 @@
 package se.lexicon.simon.arena_fighter_assignment;
 
 public class Main {
-
+	
+	public static void main (String[] args) {
+		
+		
+	}
 	
 	
 }
--- src/main/java/se/lexicon/simon/arena_fighter_assignment/Round.java
+++ src/main/java/se/lexicon/simon/arena_fighter_assignment/Round.java
@@ -1,5 +1,18 @@
 package se.lexicon.simon.arena_fighter_assignment;
 
+import se.lexicon.simon.number_generator.RandomNumberGenerator;
+
 public class Round {
 
+	public void rollDice () {
+	
+RandomNumberGenerator rng = RandomNumberGenerator.getInstance();
+		
+		int rolldice = rng.getRandomIntBetween(1, 6);
+		System.out.println(rolldice);
+		
+		
+		
+	}
+	
 }
